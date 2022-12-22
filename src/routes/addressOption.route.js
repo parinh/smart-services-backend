dc0044112c2fe90889cd9163256b5a40e6ee3311
Punyawat <punyawat89@hotmail.com>
2022-12-22 13:35:23 +0700

    test commit

--- src/routes/addressOption.route.js
+++ src/routes/addressOption.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/province/get", async function (req, res, next) {
     try {
+        //* comments
         res.json(await AddressOption.findAllProvinces())
         
     }
