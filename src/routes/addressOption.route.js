a44dc18d3e7c5563fff6c621164d1bc8b2f34c07
parin hopper <hopper.ken42@gmail.com>
2023-02-17 17:25:06 +0700

    order address , new create order_cost function for sometimes has dup order_cost

--- src/routes/addressOption.route.js
+++ src/routes/addressOption.route.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.get("/province/get", async function (req, res, next) {
     try {
-        //* commentslllllppppp
         res.json(await AddressOption.findAllProvinces())
         
     }
